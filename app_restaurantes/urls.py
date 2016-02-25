2e8e82f4d99d8c757709eb5a5d94210dc331b636
alberto__segura <alberto.segura.delgado@gmail.com>
2016-02-25 17:35:31 +0100

    bugfixes

--- app_restaurantes/urls.py
+++ app_restaurantes/urls.py
@@ -1,10 +1,10 @@
 
 
 from django.conf.urls import patterns, url
-from rango import views
+from app_restaurantes import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
 	url(r'^hello', views.helloworld, name='helloworld')
 )
 
