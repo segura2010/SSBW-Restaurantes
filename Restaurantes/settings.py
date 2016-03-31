c0d7c907a00a4000ca1e856b5ebf49f56795cd6c
alberto__segura <alberto.segura.delgado@gmail.com>
2016-03-31 18:59:48 +0200

    añadida api rest para modelos normales de Django

--- Restaurantes/settings.py
+++ Restaurantes/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_restaurantes',
     'registration',
-    'easy_maps'
+    'easy_maps',
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -136,4 +137,14 @@ REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged
 LOGIN_REDIRECT_URL = '/resta/index'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
                                                              # and are trying to access pages requiring authentication
+
+
+# Rest config
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
